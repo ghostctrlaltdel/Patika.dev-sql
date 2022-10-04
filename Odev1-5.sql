f21ccd8d7af6b9ed5d8b39b06fb1ee8586468e82
ghostctrlaltdel <114265351+ghostctrlaltdel@users.noreply.github.com>
2022-10-05 00:00:16 +0300

    Create Odev1-5.sql

---
+++ Odev1-5.sql
@@ -0,0 +1,2 @@
+-film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+SELECT *FROM film WHERE length<=50 and (rental_rate <>2.99 OR rental_rate<>4.99);
