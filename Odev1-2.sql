2f1d34ea8215b38e6910936aa6882f3b6f2f0e30
ghostctrlaltdel <114265351+ghostctrlaltdel@users.noreply.github.com>
2022-10-04 23:57:03 +0300

    Create Odev1-2.sql

---
+++ Odev1-2.sql
@@ -0,0 +1,2 @@
+-film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+select*from film where length >60 and length <75;
